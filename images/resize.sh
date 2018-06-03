e77a78d9a26c41acbeee9bfbaa4d15a29ff4bed6
German Krauss <german@devartis.com>
2018-06-03 10:23:06 -0300

    Gift list complete. New images

--- images/resize.sh
+++ images/resize.sh
@@ -1,2 +1,10 @@
 mogrify -resize x250 0*.jpg
+mogrify -resize x250 1*.jpg
+mogrify -resize x250 2*.jpg
+mogrify -resize x250 3*.jpg
+mogrify -resize x250 4*.jpg
 mogrify -gravity Center -crop 300x250+0+0 +repage 0*.jpg
+mogrify -gravity Center -crop 300x250+0+0 +repage 1*.jpg
+mogrify -gravity Center -crop 300x250+0+0 +repage 2*.jpg
+mogrify -gravity Center -crop 300x250+0+0 +repage 3*.jpg
+mogrify -gravity Center -crop 300x250+0+0 +repage 4*.jpg
